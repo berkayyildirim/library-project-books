396ebd556ed6aaddf8791e9437e0011c2567ece7
berkayyildirim <huseyinberkayyildirim@gmail.com>
2022-10-04 12:54:38 +0300

    implement referenced data model

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -1,16 +1,20 @@
 const Book = require("../models/Book.model");
+const Author = require("../models/Author.model");
 
 const router = require("express").Router();
 
 //READ: List all books
 router.get("/books", (req, res, next) => {
   Book.find()
+    .populate("author")
     .then((booksFromDB) => {
+      console.log(booksFromDB);
+
       res.render("books/books-list", { books: booksFromDB });
     })
     .catch((err) => {
       console.log("error getting books from DB", err);
-      next();
+      next(err);
     });
 });
 
